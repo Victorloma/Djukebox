44267e4d4d571dde105610e0bf5b9a5343695867
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-11-03 13:44:47 +0100

    added search function and genre selection

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -6,29 +6,20 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const TopCharts = () => {
     const { activeSong, isPlaying } = useSelector((state) => state.player);
-    const { data, isFetching, error } = useGetTopChartsQuery(country);
+    const { data, isFetching, error } = useGetTopChartsQuery();
 
-    console.log(country);
+    if (isFetching) return <Loader title='Loading top charts' />;
 
-    useEffect(() => {
-        axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_dY6kjw5wNBhHpdohPPBxT4bnvHtUh`)
-            .then((res) => setCountry(res?.data?.location?.country))
-            .catch((err) => console.log(err))
-            .finally(() => setLoading(false));
-    }, [country])
-
-    if(isFetching && loading) return <Loader title='Loading songs around you' />;
-
-    if(error && country) return <Error />;
+    if (error && country) return <Error />;
 
     return (
         <div className='flex flex-col'>
-            <h2 
+            <h2
                 className="font-bold text-3xl text-white text-left mt-4 mb-10">
-                    Around You <span className='font-black'>{country}</span> 
+                Discover Top Charts
             </h2>
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-                {data?.map((song,i)=> (
+                {data?.map((song, i) => (
                     <SongCard
                         key={song.key}
                         song={song}
